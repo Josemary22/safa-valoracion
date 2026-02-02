fc5f5157b12844fe8764a3dafc0531f314e7d2e0
josemary22 <josemarimateos2005@gmail.com>
2026-02-02 19:43:45 +0100

    Enlazado de paginas, Usuario/Admin

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -8,6 +8,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 final class AdminController extends AbstractController
@@ -18,6 +19,7 @@ final class AdminController extends AbstractController
         return $this->render('admin/admin.html.twig');
     }
 
+    #[IsGranted('ROLE_ADMIN')]
     #[Route('/admin/character/load', name: 'app_cargarDatos')]
     public function data_load(HttpClientInterface $httpClient, EntityManagerInterface $entityManager): Response
     {
--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -10,9 +10,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 final class CategoryController extends AbstractController
 {
+    #[IsGranted('ROLE_ADMIN')]
     #[Route('/category', name: 'categories')]
     public function index(Request $request,
                           CharacterRepository $characterRepository,
